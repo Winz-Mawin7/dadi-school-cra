d8f38c85c4c328dc69217780e16e5ee2385365ee
Winz <Winz.Mawin7@gmail.com>
2020-12-11 13:21:56 +0700

    Setup Tailwind & Antd.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, version } from 'react';
+import { Button, DatePicker, message } from 'antd';
 
-function App() {
+import './index.css';
+
+import logo from './logo.svg';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container pl-12 '>
+      <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4'>
+        <div className='flex-shrink-0'>
+          <img className='h-12 w-12' src={logo} alt='ChitChat Logo' />
+        </div>
+        <div>
+          <div className='text-xl font-medium text-black'>ChitChat</div>
+          <p className='text-gray-500'>You have a new message!</p>
+        </div>
+      </div>
+      <div className='App'>
+        <h1>antd version: {version}</h1>
+        <DatePicker />
+        <Button type='primary' style={{ marginLeft: 8 }}>
+          Primary Button
+        </Button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
